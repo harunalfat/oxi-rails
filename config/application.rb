4f6ab78ce6e1bb53ffd9230f4d42ca5300f98848
Alfat Saputra Harun <harunalfat@gmail.com>
2016-01-31 20:05:09 +0700

    Remove compressor

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module Myapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
     Koala.config.api_version = 'v2.0'
 
     config.before_configuration do
